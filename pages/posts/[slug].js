26aae8f0ae8979de34474cc1b3f1175c7cd1cc15
James Tucker <jamesctucker@protonmail.com>
2021-07-05 16:42:28 -0500

    Render a single post via a slug

---
+++ pages/posts/[slug].js
@@ -0,0 +1,41 @@
+import Image from 'next/image'
+import parse from 'html-react-parser';
+import { getSinglePost, getPosts } from "../../lib/posts";
+
+export async function getStaticPaths() {
+    const posts = await getPosts();
+  
+    const paths = posts.map((post) => ({
+      params: { slug: post.slug },
+    }));
+  
+    return { paths, fallback: false };
+}
+
+export async function getStaticProps(context) {
+    const post = await getSinglePost(context.params.slug);
+  
+    if (!post) {
+      return {
+        notFound: true,
+      };
+    }
+  
+    return {
+      props: { post },
+    };
+}
+
+const PostPage = ({post}) => {
+    return (
+        <article>
+            <Image src={post.feature_image} alt="Article's feature image" width={500} height={300} />
+            <h1>{post.title}</h1>
+            <p>{post.published_at}</p>
+            <p>{post.reading_time} {post.reading_time > 1 ? `minutes` : `minute`}</p>
+            <div>{parse(post.html)}</div>
+        </article>
+    )
+}
+
+export default PostPage;
