018da3a7af871eb9cab997f4849043cd2106a04f
James Tucker <jamesctucker@protonmail.com>
2021-07-09 23:57:43 -0500

    Add syntax highlighting with highlight.js

--- pages/writing/[slug].js
+++ pages/writing/[slug].js
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
 import Image from "next/image";
+// import Head from "next/head";
 import parse from "html-react-parser";
 import { getSinglePost, getPosts } from "../../lib/posts";
+import { readingTime } from "@tryghost/helpers";
+import hljs from "highlight.js";
+import "highlight.js/styles/night-owl.css";
+import javascript from "highlight.js/lib/languages/javascript";
+hljs.registerLanguage("javascript", javascript);
 
 export async function getStaticPaths() {
   const posts = await getPosts();
@@ -27,21 +34,56 @@ export async function getStaticProps(context) {
 }
 
 const PostPage = ({ post }) => {
+  useEffect(() => {
+    hljs.highlightAll();
+  }, []);
+  console.log(post);
+  const date = new Date(post.updated_at);
+  const month = date.getMonth();
+  const day = date.getDate();
+  const year = date.getFullYear();
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
   return (
-    <article>
-      <Image
-        src={post.feature_image}
-        alt="Article's feature image"
-        width={500}
-        height={300}
-      />
-      <h1 className="text-3xl font-semibold">{post.title}</h1>
-      <p>{post.published_at}</p>
-      <p>
-        {post.reading_time} {post.reading_time > 1 ? `minutes` : `minute`}
-      </p>
-      <div>{parse(post.html)}</div>
-    </article>
+    <>
+      <article className="flex flex-col justify-center">
+        {post.feature_image && (
+          <Image
+            src={post.feature_image}
+            alt="Article's feature image"
+            width={500}
+            height={300}
+          />
+        )}
+        <h1 className="text-3xl text-center font-semibold mt-2 mx-3">
+          {post.title}
+        </h1>
+        <div className="flex text-center mt-2 justify-center">
+          <p className="text-gray-400 mt-2 mb-0">
+            Updated {`${day} ${months[month - 1]} ${year}.`}&nbsp;
+          </p>
+          <p className="text-gray-400 mt-2 mb-0">
+            {readingTime(post, {
+              minute: "1 minute read.",
+              minutes: "% minute read.",
+            })}
+          </p>
+        </div>
+        <div className="mt-2">{parse(post.html)}</div>
+      </article>
+    </>
   );
 };
 
